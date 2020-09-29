6f9fc7bb6bb69b7b17b76668ff48b9a72980eb27
Serega <borzunov_sm2mail.ru>
2020-09-29 20:46:49 +0300

    Switched to touches began

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -19,26 +19,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
-        //view.backgroundColor = .cyan
+       
     }
 
 
     fileprivate func updateUI() {
-        //print(#line, #function, isLightOn)
-       /* if isLightOn {
-            view.backgroundColor = .white
-        }
-        else{
-            view.backgroundColor = .black
-            
-        }*/
+     
         view.backgroundColor = isLightOn ? .white : .black
     }
     
-    @IBAction func buttonPressed() {
-        //isLightOn = !isLightOn
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         isLightOn.toggle()
         updateUI()
     }
+    
 }
 
